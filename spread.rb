6dbee3931c061346c97e0f432a739d9b7d91a216
Andrew Gardner <agardner@LINE_TH_cd12e5.local>
2019-08-06 17:36:26 -0700

    b10 full process works, no dynamic formatting 4 new bands inserted 4 final write though

--- spread.rb
+++ spread.rb
@@ -168,7 +168,7 @@ def go_b10_a2(bandsArray)
     # COUNTER FOR LENGTH OF BANDS ARRAY FOR USE IN FUNCTIONS BELOW
     bandsLength = bandsArray.length   
     #### loadingMessage()
-=begin
+
     # DRIVERLOGIN.RB FUNCTION TO LOG INTO BACKEND AND FIND PROPER PAGE/IFRAME
     navigate($_userNameVar)                                                           
     sleep (7) 
@@ -178,7 +178,7 @@ def go_b10_a2(bandsArray)
     checkTableDownload(bandsArray) ######### Trying bandsArray instead of bandsArray (might need to switch back to bandsArray)
     browserDownloadFiles($_files_href)
     grabXlsxB10()
-=end
+
     bandObjArray = []
     bandObjArray = b10Parse(bandsArray)
 

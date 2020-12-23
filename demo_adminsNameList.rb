582da0acff973a194570a8bb42fdfc9e278304e5
MooseandSquvirrel <agardner@Wi-Fi-Looking-for-Networks.local>
2020-12-23 14:17:46 -0800

    demo version for admins

---
+++ demo_adminsNameList.rb
@@ -0,0 +1,5 @@
+# LARGE LIST REDACTED FOR DEMO
+def adminsNamesList()
+    adminsNamesList = "John Smith, Tiny Fey, Jason Mamoa".split(',')
+    adminsNamesList = adminsNamesList.map {|x| x.strip }
+end
